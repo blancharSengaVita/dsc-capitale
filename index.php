2d3ab6930e72512931042b1b9ab1d14c3337faef
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-02-12 18:31:07 +0100

    fin du devoir

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
 
+$countryName = '';
 $countries = [
 	'belgique' => [
 		'capital-name' => 'bruxelles',
-		'flag-file' => 'belgium-flag-icon-256.png'
+		'flag-file' => 'belgique-flag-icon-256.png'
 	],
 	'allemagne' => [
 		'capital-name' => 'berlin',
-		'flag-file' => 'germany-flag-icon-256.png'
+		'flag-file' => 'allemagne-flag-icon-256.png'
 	],
 	'corée du nord' => [
 		'capital-name' => 'pyongyang',
-		'flag-file' => 'north-korea-flag-icon-256.png'
+		'flag-file' => 'corée du nord-flag-icon-256.png'
 	],
 	'Afrique du sud' => [
 		'capital-name' => 'pretoria',
-		'flag-file' => 'south-africa-flag-icon-256.png'
+		'flag-file' => 'afrique du sud-flag-icon-256.png'
 	],
 ];
-
-
-
+function mb_ucfirst($string)
+{
+	return mb_strtoupper(mb_substr($string, 0, 1)).mb_substr($string, 1);
+}
 require "index.view.php";
--- index.view.php
+++ index.view.php
@@ -1,23 +1,47 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
 </head>
 <body>
-<form>
-    <label for="country"> </label>
+<form action="index.php">
+	<label for="country"> </label>
 
-    <select id="country">
-		<option value=""> Choisi un pays </option>
+	<select name="country" id="country">
+		<option value="">
+			Choisi un pays
+		</option>
 		<?php foreach ($countries as $key => $country): ?>
-		<option value="<?= $key?>"> <?= $key ?></option>`
+			<option value="<?= $key?>" >
+				<?= mb_strtoupper($key) ?>
+			</option>`
+			<?php $countryName = $_GET['country'] ?>
 		<?php endforeach; ?>
 	</select>
-	<button>donne moi la capitale</button>
+	<button type='submit'>
+		donne moi la capitale
+	</button>
 </form>
+
+
+<?php if (array_key_exists($countryName, $countries)): ?>
+<div>
+	<p>
+		<?= mb_ucfirst($countries[$countryName]['capital-name']) ?>
+	</p>
+	<img src="images/<?= $countryName ?>-flag-icon-256.png"
+		 alt="drapeau de ce pays : <?= mb_ucfirst($countryName) ?>">
+</div>
+<?php elseif ($countryName === ''): ?>
+
+<?php else: ?>
+	<p>
+		erreur
+	</p>
+<?php endif; ?>
 </body>
 </html>
 
